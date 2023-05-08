e040dc8ff85b23eb78fc86be9806181a7e32571c
Isaiah Weidl <isaiah.weidl@gmail.com>
2023-05-08 20:24:23 +1000

    Get Current Location implemented.

--- app/src/main/java/com/iweidl/assignment71p/CreateNewActivity.java
+++ app/src/main/java/com/iweidl/assignment71p/CreateNewActivity.java
@@ -1,15 +1,30 @@
 package com.iweidl.assignment71p;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.google.android.gms.location.LocationServices;
 import com.iweidl.assignment71p.sqlitehelper.LostFoundDBHelper;
+
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class CreateNewActivity extends AppCompatActivity {
@@ -20,10 +35,26 @@ public class CreateNewActivity extends AppCompatActivity {
     private EditText editTextDate;
     private EditText editTextLocation;
     private Button buttonSave;
+    private Button buttonGetCurrentLocation;
+    Location currentLocation;
 
     private LostFoundDBHelper dbHelper;
 
     SimpleDateFormat dateFormat;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 0, locationListener);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +69,8 @@ public class CreateNewActivity extends AppCompatActivity {
         editTextLocation = findViewById(R.id.editTextLocation);
         buttonSave = findViewById(R.id.buttonSave);
 
+        buttonGetCurrentLocation = findViewById(R.id.buttonGetCurrentLocation);
+
         // Define a format to be used for parsing editTextDate
         dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
         // Set editTextDates to today's date by default.
@@ -46,9 +79,46 @@ public class CreateNewActivity extends AppCompatActivity {
         // Initialize the dbHelper for use later
         dbHelper = new LostFoundDBHelper(this);
 
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 0, locationListener);
+
         // saveItem is called when buttonSave is tapped
         buttonSave.setOnClickListener(view -> {
             saveItem();
+        });
+
+        buttonGetCurrentLocation.setOnClickListener(view -> {
+            currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            editTextLocation.setText(currentLocation.toString());
+
+            if (currentLocation != null) {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
+                    if (addressList != null && !addressList.isEmpty()) {
+                        Address address = addressList.get(0);
+                        editTextLocation.setText(address.getAddressLine(0));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         });
     }
 
